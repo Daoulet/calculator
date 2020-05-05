bc1f19210e25ae289bc46dc44a54d7cbecb679a6
Daulet Bimaganbetov <bimaganbetov1995@gmail.com>
2020-05-05 22:44:16 +0600

    2nd

--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ def home_page(request):
 	return render(request, 'home.html')
 
 def add(request):
-	val1 = int(request.GET['num1'])
-	val2 = int(request.GET['num2'])
-	res = val1 + val2
-	return render(request, "result.html", {'result': res})
+	val1 = int(request.GET['salary'])
+	pension = val1*0.1
+	tax = (val1-(val1*0.1)-42500)*0.1
+	sum1 = val1-(val1*0.1)-tax
+	return render(request, "result.html", {'result': pension, 'result2': tax, 'result3': sum1})
